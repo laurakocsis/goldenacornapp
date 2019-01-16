3e3a60bf5a6d1192db07a5ec063e1d8580a8a6a6
laurakocsis <laura.kocsis.95@gmail.com>
2019-01-16 17:08:35 +0100

    feat(goldenacorn): prop types added

--- src/Button.js
+++ src/Button.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Button = (props) => {
   return (
-      <button onClick={props.clicked}>{props.value}</button>
+    <button onClick={props.clicked}>{props.value}</button>
   )
+}
+
+Button.propTypes = {
+  clicked: PropTypes.func.isRequired,
+  value: PropTypes.string
 }
 
 export default Button;
--- src/Display.js
+++ src/Display.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Display = (props) => {
   return (
     props.children
   )
+}
+
+Display.propTypes = {
+  children: PropTypes.element.isRequired
 }
 
 export default Display;
