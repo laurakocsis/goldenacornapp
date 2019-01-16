63f2d275425eb18f81da59e7387d3a2003d91c0f
laurakocsis <laura.kocsis.95@gmail.com>
2019-01-16 12:18:07 +0100

    feat(goldenacorn): missing setups added

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,10 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env'],
+          plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/plugin-proposal-class-properties']
+        }
       },
     ],
   },
