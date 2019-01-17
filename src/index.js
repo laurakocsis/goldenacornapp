c6586cc9c32549614e0d912bc74d0f5c44f1a544
laurakocsis <laura.kocsis.95@gmail.com>
2019-01-17 15:06:18 +0100

    feat(goldenacorn): provider added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './reducers/reducer';
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root'),
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
 );
