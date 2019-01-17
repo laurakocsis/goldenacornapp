aa5187e468d913d8de466e285a53da5c022b6ffa
laurakocsis <laura.kocsis.95@gmail.com>
2019-01-17 15:07:58 +0100

    fix(goldenacorn): cases corrected to strings

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,12 +1,14 @@
 import { combineReducers } from 'redux';
 
-function acorns(state = { counter: 0 }, action) {
+const acorns = (state = { counter: 0 }, action) => {
   switch (action.type) {
-    case BUY_ACORN:
+    case 'BUY_ACORN':
       return { ...state, counter: state.counter + action.payload }
-    case EAT_ACORN:
-      if (conuter > 1) {
-        return { ...state, counter: state.counter + action.payload }
+    case 'EAT_ACORN':
+      if (counter > 0) {
+        return { ...state, counter: state.counter - action.payload }
+      } else {
+        return state
       }
     default:
       return state
