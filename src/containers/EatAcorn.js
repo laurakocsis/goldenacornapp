8dee74890292d58fbd035fc0c1cd2473a2042a82
laurakocsis <laura.kocsis.95@gmail.com>
2019-01-17 15:09:45 +0100

    feat(goldenacorn): EatAcorn container created

---
+++ src/containers/EatAcorn.js
@@ -0,0 +1,24 @@
+import { connect } from 'react-redux';
+import { eatAcorn } from '../actions';
+import Button from '../components/Button';
+
+const mapStateToProps = () => {
+  return {
+    value: 'Eat one',
+  };
+};
+
+const mapDispatchToProps = () => {
+  return {
+    clicked: () => {
+      dispatch(eatAcorn(1));
+    },
+  };
+};
+
+const EatAcorn = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Button);
+
+export default EatAcorn;
