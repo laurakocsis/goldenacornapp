befef03414e1ce2b785b61d5fe3246f65baa810b
laurakocsis <laura.kocsis.95@gmail.com>
2019-01-18 12:08:36 +0100

    feat(goldenacorn): routes added, importing paths changed

--- src/index.js
+++ src/index.js
@@ -3,13 +3,24 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import rootReducer from './reducers/reducer';
+import Navbar from './components/navbar';
+import Home from './components/Home';
+import ReduxApp from './components/ReduxApp';
 
 const store = createStore(rootReducer);
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <Router>
+    <Provider store={store}>
+      <App>
+        <Navbar />
+        <Route exact path='/' component={Home} />
+        <Route path='/redux' component={ReduxApp} />
+        <Route path='/state' component={States} />
+      </App>
+    </Provider>
+ </Router>,
   document.getElementById('root')
 );
