4edd013e815daebd8d51c7531209316f67ee6c0d
laurakocsis <laura.kocsis.95@gmail.com>
2019-01-18 12:25:15 +0100

    refact(goldenacorn): route as the only children of App

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import rootReducer from './reducers/reducer';
-import Navbar from './components/navbar';
 import Home from './components/Home';
 import ReduxApp from './components/ReduxApp';
+import StatesApp from './components/StatesApp';
 
 const store = createStore(rootReducer);
 
@@ -15,10 +15,9 @@ ReactDOM.render(
   <Router>
     <Provider store={store}>
       <App>
-        <Navbar />
         <Route exact path='/' component={Home} />
         <Route path='/redux' component={ReduxApp} />
-        <Route path='/state' component={States} />
+        <Route path='/states' component={StatesApp} />
       </App>
     </Provider>
  </Router>,
